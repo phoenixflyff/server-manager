40765d18a1170ce4c62271277e2009f82677eb69
deadphoenix8091 <41997317+deadphoenix8091@users.noreply.github.com>
2021-02-06 18:28:16 +0100

    integrate basic webserver

--- server-manager/Program.cs
+++ server-manager/Program.cs
@@ -1,5 +1,9 @@
 ﻿using server_manager.Utils;
-using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
 
 namespace server_manager
 {
@@ -7,7 +11,51 @@ namespace server_manager
     {
         static void Main(string[] args)
         {
-            Logger.LogMessage(Logger.LogLevel.INFO, "Hello {0}", "World");
+            Logger.LogMessage(Logger.LogLevel.INFO, "Server Manager is starting.");
+            Logger.LogMessage(Logger.LogLevel.INFO, "Build: {0}", (
+              ThisAssembly.Git.SemVer.Major + "." +
+              ThisAssembly.Git.SemVer.Minor + "." +
+              ThisAssembly.Git.SemVer.Patch + "-" +
+              ThisAssembly.Git.Branch + "+" +
+              ThisAssembly.Git.Commit));
+
+            Logger.LogMessage(Logger.LogLevel.INFO, "Starting Webserver.");
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddMvcCore();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+
+    public class MyEndpoint : Controller
+    {
+        [Route("")]
+        public IActionResult Get()
+        {
+            return new OkResult();
         }
     }
 }
